980969319d3b267613a5001b58dc16735580cb10
Parth Doshi <doship2468@gmail.com>
2019-06-26 15:22:23 -0700

    First

---
+++ src/main/java/com/github/doshiparth/kafka/tweetspolling/TwitterProducer.java
@@ -0,0 +1,7 @@
+package com.github.doshiparth.kafka.tweetspolling;
+
+public class TwitterProducer {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
